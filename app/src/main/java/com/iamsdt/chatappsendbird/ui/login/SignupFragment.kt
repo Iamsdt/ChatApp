814093cea1111813b552a3ff1cb90e24cf8d1b67
Iamsdt <shudiptotrafder@gmail.com>
2018-05-25 23:30:14 +0600

    design

--- app/src/main/java/com/iamsdt/chatappsendbird/ui/login/SignupFragment.kt
+++ app/src/main/java/com/iamsdt/chatappsendbird/ui/login/SignupFragment.kt
@@ -18,7 +18,7 @@ class SignupFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        return inflater.inflate(R.layout.signup_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
