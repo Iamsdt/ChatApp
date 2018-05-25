4190d7fd7403514787b2bc9f5ac939e4fcea6f8b
Iamsdt <shudiptotrafder@gmail.com>
2018-05-25 15:59:05 +0600

    first commit

---
+++ app/src/main/java/com/iamsdt/chatappsendbird/SpUtils.kt
@@ -0,0 +1,16 @@
+package com.iamsdt.chatappsendbird
+
+import android.content.Context
+
+class SpUtils {
+    private var context: Context? = null
+
+    fun initilize(context: Context) {
+        this.context = context
+    }
+
+    fun isAppRunningForFristTime(context: Context){
+        return
+    }
+
+}
---
+++ app/src/main/java/com/iamsdt/chatappsendbird/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.iamsdt.chatappsendbird
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
